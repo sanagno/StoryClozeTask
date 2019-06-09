5b403b15d9fccb951c075879f4f1af6d33728d45
Adamos Solomou <solomoua@lo-login-02.leonhard.ethz.ch>
2019-06-09 02:25:09 +0200

    fixes

--- src/model.py
+++ src/model.py
@@ -63,9 +63,7 @@ class NLUModel():
         Returns: a float corresponding to the accuracy of the model.
         """
         score = accuracy_score(y_true, y_pred)
-<<<<<<< HEAD
-        return score
-=======
+
         return score
 
->>>>>>> 1cfe53c346828791ad5bb1706234a1fc79f1fbec
+
