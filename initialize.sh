5acc05d93661fc0a8eabdeb6731848d7cb2a3b6d
Sotirios Anagnostidis <sanagnos@lo-login-02.leonhard.ethz.ch>
2019-06-10 16:47:27 +0200

    initialize

---
+++ initialize.sh
@@ -0,0 +1,9 @@
+# load modules
+module load gcc/4.8.5 python_gpu/3.6.4 hdf5 eth_proxy
+module load cudnn/7.2
+
+# install pacakges
+pip install --user virtualenvwrapper
+source $HOME/.local/bin/virtualenvwrapper.sh
+mkvirtualenv "nlu-project2"
+
