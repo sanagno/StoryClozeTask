925d45b1f8290cc73e9570ecbcb7af2ab4135e3f
YannisSach <yannissah@yahoo.gr>
2019-06-11 19:14:28 +0200

    Create virtualenv with venv

--- initialize.sh
+++ initialize.sh
@@ -2,9 +2,12 @@
 module load gcc/4.8.5 python_gpu/3.6.4 hdf5 eth_proxy
 module load cudnn/7.2
 
+# pip install --user virtualenvwrapper
+# source $HOME/.local/bin/virtualenvwrapper.sh
+# mkvirtualenv "nlu-project2"
+
 # install pacakges
-pip install --user virtualenvwrapper
-source $HOME/.local/bin/virtualenvwrapper.sh
-mkvirtualenv "nlu-project2"
+python -m venv "nlu-project2"
+source nlu-project2/bin/activate
 
 export PATH=$PATH:/$PWD
